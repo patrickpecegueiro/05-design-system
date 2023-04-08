fe033e01b66f2a17427b1ed6718d123965e4a2b3
Patrick Pecegueiro <patrickpecegueiro@Patricks-MacBook-Pro.local>
2023-04-08 10:11:27 -0400

    feat: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/patrickpecegueiro.png',
+    alt: 'Patrick Pecegueiro',
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
